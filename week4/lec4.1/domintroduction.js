7ec1eadd266b63228c0ff85808786ba89887eedd
chandrabhan4122003 <chandrabhan41222003@gmail.com>
2025-07-10 12:54:20 +0530

    done dom

---
+++ week4/lec4.1/domintroduction.js
@@ -0,0 +1,9 @@
+// what was javascript 
+// it was an implementation of the ECMA Script spec
+// but the javascript that runs in your browser has some extra functionality 
+
+//  setTimrout fetch setInterval document are not teh part of the js spec  : Browser js   
+// setTimeout http fs are not the part of js spec :Node js 
+
+
+// 
